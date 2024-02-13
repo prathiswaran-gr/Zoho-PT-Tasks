96e733ae7104c98139cc2919170f6efa03d66406
prathiswaran-gr <prathis2003@gmail.com>
2024-02-13 11:04:24 +0530

    changed while loop to for-each loop in Task8

--- Day2/Task8/RemovePropertiesWhileEnumerating.java
+++ Day2/Task8/RemovePropertiesWhileEnumerating.java
@@ -2,21 +2,25 @@ import java.util.Properties;
 import java.util.Enumeration;
 
 public class RemovePropertiesWhileEnumerating {
+
+  public static void displayProperties(Properties props){
+  	for(String key : props.stringPropertyNames()){
+      		System.out.println(key + " = " + props.getProperty(key));
+    	}
+  }
   public static void main(String[] a) {
 
     Properties props = new Properties();
     props.setProperty("name","prathis");
     props.setProperty("age","20");
     
-    Enumeration e = props.propertyNames();
-
-    while (e.hasMoreElements()) {
-      String key = (String) e.nextElement();
-      if(key.equals("age")){
-      	props.remove(key);
-      }
-      System.out.println(key + " = " + props.getProperty(key));
+   // Enumeration e = props.propertyNames();
+    for(String key : props.stringPropertyNames()){
+    	if(key.equals("age")){
+      		props.remove(key);
+      	}
     }
+    displayProperties(props); // prints the properties available in the object.
     
   }
 }
